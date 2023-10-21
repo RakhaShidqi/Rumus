f3e6a905d6d5d4f7542b5a62fdb3fb993a674438
RakhaShidqi <48802289+RakhaShidqi@users.noreply.github.com>
2023-10-21 17:36:09 +0700

    tambah fitur rumus persegi

---
+++ Rumus Penghitung luas dan keliling Persegi.py
@@ -0,0 +1,19 @@
+print(" #===========================================================================#")
+print(" #================       Aplikasi Penghitung luas persegi    ================#")
+print(" #================             dan keliling Persegi          ================#")
+print(" #===========================================================================#")
+print(" #================ Developed and maintained by RakhaShidqi ==================#")
+print("                                                                              ")
+
+#Fungsi Untuk masukkan input jari jari dan tinggi
+sisi = float(input("Masukkan  sisi Persegi: "))
+print("                                                                              ")
+
+#Fungsi Untuk Membuat Rumus
+keliling = 4*sisi
+luas     = sisi*sisi
+
+#Fungsi Untuk Mengeluarkan Outputnya
+print("Jadi keliling Persegi Adalah: ",keliling)
+print("Jadi Luas Persegi Adalah: ",luas)
+print("                                                                              ")
